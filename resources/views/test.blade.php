eca404182c09fab73d72657802c75b4292b88a62
Benedict Mwanga <mwangaben@gmail.com>
2017-03-03 02:10:16 +0000

    testing code

---
+++ resources/views/test.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Mwangaben</title>
+        <link rel="stylesheet" href="/css/app.css" type="text/css" />
+    </head>
+    <body>
+        
+        <div class="container">
+            <h1>Hello World</h1>
+        </div>
+        
+        <script type="text/javascript" src="/js/app.js"></script>
+    </body>
+</html>
